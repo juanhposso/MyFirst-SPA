96031f0e12264b368f787c295cf8e456996c3d77
Juan David hernandez Posso <Juan.hernandez0793@gmail.com>
2021-02-13 03:45:02 -0500

    despues de crear los templates

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+const home = () => {
+	const view = `
+	<div class="Characters">
+		<article class="Character-item">
+			<a href=#/1/>
+				<img src="image" alt="name"/>
+				<h2>Name</h2>
+			</a>
+		</article>
+	</div>`;
+	return view;
+};
+
+export default home;
